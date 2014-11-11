94c9bf89a098189628841cc481fc1e6cacafdbe4
acbui <acqbui@gmail.com>
2014-11-11 05:56:31 -0800

    First push

---
+++ migs2014/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public int lives; 
+	public bool holdingItem; 
+
+	// Use this for initialization
+	void Start () {
+		lives = 2;
+		holdingItem = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+	}
+
+}
