bfbc972a0f0c0fbfb2540924743ee044bec2b3b1
acbui <acqbui@gmail.com>
2014-11-11 07:19:25 -0800

    Scripts

--- migs2014/Assets/Scripts/Enemy.cs
+++ migs2014/Assets/Scripts/Enemy.cs
@@ -4,14 +4,118 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
 	public bool lookingAway;
+	public float currentHunger;
+	public float maxHunger;
+
+	public Animator anim;
+	public Player player;
+
+	public float minDelay;
+	public float maxDelay;
 
 	// Use this for initialization
 	void Start () {
-		
+		lookingAway = false;
+
+		player = GameObject.Find ("Player").GetComponent<Player>() as Player;
+		anim = gameObject.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		catchSteal ();
+		StartCoroutine (lookAway (Random.Range (minDelay, maxDelay)));
+	}
+
+	void catchSteal()
+	{
+		if (!lookingAway)
+		{
+			if (player.stealingItem)
+			{
+				if (player.lives == 2)
+				{
+					player.lives = 1;
+					player.foodStock = 0;
+					anim.SetInteger ("Angry", 1);
+					StartCoroutine (backToIdle (0.5f));
+				}
+				else 
+				{
+					player.lives = 0;
+					anim.SetInteger ("Angry", 2);
+					StartCoroutine (backToIdle (0.5f));
+				}
+			}
+		}
+	}
+
+	void hungerLevel()
+	{
+		if (player.foodCart + player.foodStock >= player.maxFood)
+		{
+			currentHunger = maxHunger; 
+		}
+
+		if (currentHunger >= maxHunger)
+		{
+			if (player.foodCart <= 0)
+			{
+				if (player.lives == 2)
+				{
+					player.lives = 1;
+					player.foodStock = 0;
+					anim.SetInteger ("Angry", 1);
+					StartCoroutine (backToIdle (0.5f));
+				}
+				else 
+				{
+					player.lives = 0;
+					anim.SetInteger ("Angry", 2);
+					StartCoroutine (backToIdle (0.5f));
+				}
+			}
+			else {
+				player.sendingCart = true;
+				anim.SetInteger ("Eat", 1);
+			}
+		}
+		else 
+		{
+			currentHunger++;
+		}
+	}
+
+	IEnumerator lookAway(float pDelay)
+	{
+		yield return new WaitForSeconds (pDelay);
+		lookingAway = true;
+		anim.SetInteger ("Look", 1);
+	}
+
+	IEnumerator backToIdle(float pDelay)
+	{
+		yield return new WaitForSeconds(pDelay);
+		anim.SetInteger ("Angry", 0);
+		if (player.lives == 0)
+		{
+			GameManager.ins.endGame();
+		}
+	}
+
+	IEnumerator lookBack(float pDelay)
+	{
+		yield return new WaitForSeconds(pDelay);
+		anim.SetInteger ("Look", 0);
+		lookingAway = false;
+	}
+
+	IEnumerator stopEating(float pDelay)
+	{
+		yield return new WaitForSeconds(pDelay);
+		anim.SetInteger ("Eat", 0);
+		currentHunger -= ((float)player.foodCart / (float)player.maxFood)*maxHunger;
+		player.foodCart = 0;
+		player.sendingCart = false;
 	}
 }
